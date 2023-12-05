6844cd95e4efb16d0b3c22d91b077ed7d24cfd0c
jingjingshenye <1976442140@qq.com>
2023-12-06 06:40:22 +0800

    add

---
+++ bin/deploy.bat
@@ -0,0 +1,13 @@
+call yarn build
+
+call cp -r ./dist ./doc
+
+cd doc
+
+git init
+git add -A
+git commit -m "auto construct blog"
+
+@REM  git push [远程库名] [本地分支名]:[远程分支名]
+@REM git push -f https://github.com/jingjingshenye/vitepress-blog.git master:gh-pages
+git push -f https://github.com/jingjingshenye/threejs-blog.git master:main
